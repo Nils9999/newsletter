aee869929b1535ef766b540d7c0de792c7800062
Nils9999 <nils.malcher@volkswagen.de>
2025-02-03 10:26:30 +0100

    third commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router";
+
+export function Navbar() {
+  <nav>
+    <ul>
+      <li>
+        <NavLink to={"newsletter"}>Newsletter</NavLink>
+      </li>
+    </ul>
+  </nav>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { Newsletter } from "./components/newsletter";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/home",
+        element: <Newsletter />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
