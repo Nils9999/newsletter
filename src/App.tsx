b9314b39477edfd31b2a38a01ae6ea320f2311a5
HBK-07 <hbkoca@web.de>
2025-02-03 09:28:36 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import backgroundImage from "./assets/backgroundimage.jpg";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex min-h-screen items-center justify-center bg-blue-400">
+      <div
+        className="h-[90vh] w-full max-w-7xl bg-cover bg-center p-4 shadow-lg"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+        }}
+      ></div>
+    </div>
+  );
 }
-
-export default App
